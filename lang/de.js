c62a887cf7bf96a095a133261375492d7116970b
Steffen <steffenb198@aol.com>
2015-08-13 23:19:04 +0200

    Remove incomplete install instructions and only link to the wiki

--- lang/de.js
+++ lang/de.js
@@ -29,11 +29,6 @@
         "inno": "Für ein fantastisches Spiel & die Ermöglichung dieses Projekts"
     },
     "steps": {
-        "extract": "Entpacke das heruntergeladene Archiv",
-        "launcher": "Starte den Launcher und gehe auf deine lokale IP-Adresse<br>(Der Launcher wird dir mehrere mögliche Adressen anzeigen)",
-        "firstuser": "Der erste registrierte Benutzer über <a href=\"http://127.0.0.1\">localhost</a> erhält automatisch Adminrechte",
-        "play": "Viel Spaß beim Spielen",
-        "post": "Melde Fehler mit <b>Screenshots und ausführlichen Schritten zum Reproduzieren</b><br>(bevorzugterweise <a href=\"https://github.com/TWLan/Issues/issues\">hier</a>)",
         "detailed": "Klicke hier für detailierte Anweisungen"
     },
     "license": "Durch das Benutzen dieser Software erklären Sie sich mit den Bedingungen der <a href=\"https://github.com/TWLan/Wiki/wiki/License\">TWLan Lizenz</a> einverstanden, sowie mit den Lizenzen aller mitgelieferter Software.<br>Alle Lizenzen können dem \"license\" Ordner innerhalb des Download-Archivs entnommen werden."
--- lang/en.js
+++ lang/en.js
@@ -29,11 +29,6 @@
         "inno": "For a fantastic game and making this project possible"
     },
     "steps": {
-        "extract": "Extract the downloaded archive",
-        "launcher": "Start the launcher and go to your local IP-address<br>(the launcher will show you several URLs)",
-        "firstuser": "The first user who registers from <a href=\"http://127.0.0.1\">localhost</a> receives admin privileges",
-        "play": "Play the game and have fun",
-        "post": "Report errors with <b>screenshots and detailed steps to reproduce</b><br>(preferably <a href=\"https://github.com/TWLan/Issues/issues\">here</a>)",
         "detailed": "Click here for detailed instructions"
     },
     "license": "By using this software, you agree to the terms of the <a href=\"https://github.com/TWLan/Wiki/wiki/License\">TWLan License</a> and to the licenses of all the software it uses.<br>All individual licenses can be found in the \"license\" folder inside the download archives."
